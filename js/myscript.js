18e47c502213296353a272590e6901c88363027d
Donatella7 <campaniellodonatella@gmail.com>
2022-03-24 15:49:22 +0100

    cmt

--- js/myscript.js
+++ js/myscript.js
@@ -16,13 +16,11 @@ const app = new Vue({
     },
     
     mounted() {
-
         const self = this;
         for(let i = 0; i < 10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then(function (response){
                 self.emailArray.push(response.data.response);
-                console.log(self.emailArray);
             });
         }
 
